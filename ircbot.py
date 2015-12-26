3606ac375a6378204da62429862beb8203221021
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2015-12-26 03:54:49 -0800

    Added main() and docstrings

--- ircbot.py
+++ ircbot.py
@@ -8,45 +8,53 @@ server  = ""
 channel = ""
 botnick = ""
 
-# Ping the server
 def ping():
+  """Ping the server."""
+
   ircsock.send(bytes("PONG :pingis\n", 'utf-8'))  
 
-# Send message to the server
-def sendMessage(chan , msg):
-  ircsock.send(bytes("PRIVMSG "+ chan +" :"+ msg +"\n", 'utf-8')) 
+def sendMessage(chan, msg):
+  """Send message to the server."""
 
-# Join a channel
+  ircsock.send(bytes("PRIVMSG "+ chan +" :"+ msg +"\n", 'utf-8'))
+
 def joinChannel(chan):
+  """Join a channel."""
+
   ircsock.send(bytes("JOIN "+ chan +"\n", 'utf-8'))
 
-# Send "hello world" message to server
 def helloWorld():
+  """Send "hello world" message to the server."""
+
   ircsock.send(bytes("PRIVMSG "+ channel +" :Hello world!\n", 'utf-8'))
             
-# Connect to the server                  
-ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ircsock.connect((server, 6667))
+def main():
+  # Connect to the server                  
+  ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  ircsock.connect((server, 6667))
 
-# Send a greeting message
-msg = bytes("USER " + botnick + " " + botnick + " " + botnick + 
-  "Hello, this is an IRC bot!\n", 'utf-8')
-ircsock.send(msg)
-msg = bytes("NICK "+ botnick + "\n", 'utf-8')
-ircsock.send(msg)
+  # Send a greeting message
+  msg = bytes("USER " + botnick + " " + botnick + " " + botnick + 
+    "Hello, this is an IRC bot!\n", 'utf-8')
+  ircsock.send(msg)
+  msg = bytes("NICK "+ botnick + "\n", 'utf-8')
+  ircsock.send(msg)
 
-joinChannel(channel)
+  joinChannel(channel)
 
-# Infinite loop reads bytes from server and responds to a specific message
-while True:
-  msg = ircsock.recv(recvBytes)
-  msg = ircmsg.strip(bytes('\n\r', 'utf-8'))
-  print(msg)
+  # Infinite loop reads bytes from server and responds to a specific message
+  while True:
+    msg = ircsock.recv(recvBytes)
+    msg = ircmsg.strip(bytes('\n\r', 'utf-8'))
+    print(msg)
 
-  # sendMessage(channel, "Hello!")
+    # sendMessage(channel, "Hello!")
 
-  if msg.find(bytes(":Hello " + botnick, 'utf-8')) != -1:
-    helloWorld()
+    if msg.find(bytes(":Hello " + botnick, 'utf-8')) != -1:
+      helloWorld()
 
-  if ircmsg.find(bytes("PING :", 'utf-8')) != -1:
-    ping()
+    if ircmsg.find(bytes("PING :", 'utf-8')) != -1:
+      ping()
+
+while __name__ == "__main__":
+  main()
